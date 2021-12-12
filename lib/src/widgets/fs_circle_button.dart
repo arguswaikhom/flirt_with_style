8759eeb2fdf22e4583c4737898311a421eab61ad
Argus Waikhom <realarguswaikhom@gmail.com>
2021-12-12 19:07:37 +0530

    Background color support in the FSCircleButton

--- lib/src/widgets/fs_circle_button.dart
+++ lib/src/widgets/fs_circle_button.dart
@@ -4,6 +4,7 @@ class FSCircleButton extends StatelessWidget {
   final Widget child;
   final EdgeInsets margin;
   final EdgeInsets padding;
+  final Color backgroundColor;
   final GestureTapCallback? onTap;
 
   const FSCircleButton({
@@ -12,6 +13,7 @@ class FSCircleButton extends StatelessWidget {
     this.margin = EdgeInsets.zero,
     this.padding = EdgeInsets.zero,
     this.onTap,
+    this.backgroundColor = Colors.transparent,
   }) : super(key: key);
 
   @override
@@ -19,7 +21,7 @@ class FSCircleButton extends StatelessWidget {
     return Material(
       shape: CircleBorder(),
       clipBehavior: Clip.hardEdge,
-      color: Colors.transparent,
+      color: backgroundColor,
       child: InkWell(
         onTap: onTap,
         child: Container(
